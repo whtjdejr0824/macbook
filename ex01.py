4e5888d4cedba546ad301beb765f9b7bd132dded
whtjdejr0824 <67144240+whtjdejr0824@users.noreply.github.com>
2021-01-20 23:14:25 +0900

    210120

---
+++ ex01.py
@@ -0,0 +1,4 @@
+print("hello world")
+print(2 ** 3.0)
+print(int("3")+ float("5"))
+print(str(4.0)*2)
---
+++ ex03.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 정의 (자료형 잘 선정해서)
+        // 코드를 입력하세요.
+        String name;
+        int year;
+        int month;
+        int day;
+        boolean married;
+        
+        // 값 대입
+        // 코드를 입력하세요.
+        neme = "코드잇";
+        year = 2015;
+        month = 7;
+        day = 20;
+        married = false;
+
+        // 자기 소개
+        // 코드를 입력하세요.
+        System.out.println("이름 : " + neme);
+        System.out.println("생일 : " + year + "년" + month +"월" + day + "일");
+        System.out.println("결혼 : " + married);
+    }
+}
